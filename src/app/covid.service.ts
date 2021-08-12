69b1e0b1941a3ba13648ce4da63d3c1d32417602
Shubham <bhagatshubham1994@gmail.com>
2021-08-13 00:50:43 +0530

    files cleaned up

--- src/app/covid.service.ts
+++ src/app/covid.service.ts
@@ -6,8 +6,10 @@ import { Injectable } from '@angular/core';
 })
 export class CovidService {
 
-  covidData = 'https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqa1VYSEk3cWhVdmUtZmtaam1sbDh2bnROV015QXxBQ3Jtc0tuVUV1WDEzSFVidTdDQVJyOXZHb2dSVW9rbHpocXVCOXRWZnUyaV9ZakdqdlhTcVdfMXc1TU1LZWtfQXdWUFc1TFU3ZmNQX18yV2VYU2pvb05GOUR5clhMNndPMl9pQ1pfV1h1RDh6SkVuYjF6RzlpYw&q=https%3A%2F%2Fwww.cdc.gov%2Fcoronavirus%2F2019-ncov%2Fmap-data-cases.csv';
+  // Remote CSV File
+  remoteCovidData = 'https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqa1VYSEk3cWhVdmUtZmtaam1sbDh2bnROV015QXxBQ3Jtc0tuVUV1WDEzSFVidTdDQVJyOXZHb2dSVW9rbHpocXVCOXRWZnUyaV9ZakdqdlhTcVdfMXc1TU1LZWtfQXdWUFc1TFU3ZmNQX18yV2VYU2pvb05GOUR5clhMNndPMl9pQ1pfV1h1RDh6SkVuYjF6RzlpYw&q=https%3A%2F%2Fwww.cdc.gov%2Fcoronavirus%2F2019-ncov%2Fmap-data-cases.csv';
 
+  // Local CSV file
   localCovidData = '../assets/map-data-cases.csv'
 
   constructor(private http: HttpClient) { }
